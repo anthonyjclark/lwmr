93f9ebc359691acf0f3f1d527d61cda7644dc36f
anthonyjclark <anthonyjclark@gmail.com>
2026-05-14 17:28:39 -0700

    Wroking main demo.

--- examples/basic_gym.py
+++ examples/basic_gym.py
@@ -1,19 +1,48 @@
-import gymnasium as gym
-import lwmr  # noqa: F401
+from dataclasses import dataclass
 
-SEED = 47
-STEPS = 1000
 
-env = gym.make("lwmr/Lwmr-v0", render_mode="human")
+def main(seed: int, steps: int, quiet: bool) -> None:
+    # NOTE: moving imports here to better handle quiet mode and speed up imports on help
+    import gymnasium as gym
+    import lwmr  # noqa: F401
 
-observation, info = env.reset(seed=SEED)
+    env = gym.make("lwmr/Lwmr-v0", render_mode="viser", max_episode_steps=steps, quiet=quiet)
+    # TODO: consider vectorised environments
+    # vec_env = gym.make_vec(..., n_envs=...)
 
-for _ in range(STEPS):
-    action = env.action_space.sample()
+    observation, info = env.reset(seed=seed)
+    # TODO: might still need to limit total number of steps (how is max_episode_steps handled in vectorised envs? or single environments)
+    for step in tqdm.trange(steps):
+        action = env.action_space.sample()
 
-    observation, reward, terminated, truncated, info = env.step(action)
+        observation, reward, terminated, truncated, info = env.step(action)
+        # print(observation[:3])
+        env.render()
 
-    if terminated or truncated:
-        observation, info = env.reset()
+        if terminated or truncated:
+            observation, info = env.reset()
+            break
 
-env.close()
+    env.close()
+
+
+@dataclass
+class Args:
+    """Example using Lwmr environment."""
+
+    quiet: bool = False
+    seed: int = 47
+    steps: int = 80
+
+
+if __name__ == "__main__":
+    import tqdm
+    import tyro
+    import warp as wp
+
+    args = tyro.cli(Args)
+
+    if args.quiet:
+        wp.config.quiet = True
+
+    main(seed=args.seed, steps=args.steps, quiet=args.quiet)
