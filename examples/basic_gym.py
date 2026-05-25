cf25c7423fa14df9e61ef376b189ba1c219ec03b
anthonyjclark <anthonyjclark@gmail.com>
2026-05-25 11:39:22 -0700

    Update control handling in basic gym example.

--- examples/basic_gym.py
+++ examples/basic_gym.py
@@ -1,3 +1,4 @@
+from collections.abc import Generator
 from dataclasses import dataclass
 
 import tyro
@@ -14,12 +15,44 @@ class Args:
     max_steps: int | None = None
 
 
+MotorCommand = tuple[float, float, float, float]
+
+
+def looping_control_sequence_generator(
+    lo: float, mid: float, hi: float, steps_per_command: int | list[int] = 40
+) -> Generator[MotorCommand, None, None]:
+
+    if isinstance(steps_per_command, int):
+        steps_per_command = [steps_per_command] * 5
+
+    control_sequence = [
+        (steps_per_command[0], (hi, hi, hi, hi)),  # forward
+        (steps_per_command[1], (mid, hi, mid, hi)),  # veer left
+        (steps_per_command[2], (hi, hi, hi, hi)),  # forward
+        (steps_per_command[3], (hi, mid, hi, mid)),  # veer right
+        (steps_per_command[4], (lo, lo, lo, lo)),  # stop
+    ]
+
+    control_iter = iter(control_sequence)
+    current_control_steps, current_control = next(control_iter)
+    while True:
+        yield current_control
+
+        current_control_steps -= 1
+        if current_control_steps <= 0:
+            try:
+                current_control_steps, current_control = next(control_iter)
+            except StopIteration:
+                control_iter = iter(control_sequence)
+                current_control_steps, current_control = next(control_iter)
+
+
 def main(args: Args) -> None:
     # NOTE: moving imports here to better handle quiet mode and speed up imports on help
     import gymnasium as gym
     import lwmr  # noqa: F401
     import warp as wp
-    from tqdm.auto import tqdm, trange
+    from tqdm.auto import trange
 
     if args.quiet:
         wp.config.quiet = True
@@ -32,28 +65,12 @@ def main(args: Args) -> None:
         device=args.device,
     )
 
-    # TODO: consider vectorised environments
-    # vec_env = gym.make_vec(..., n_envs=...)
-
-    hih = 12.0
-    mid = 4.0
-    off = 0.0
-    control_sequence = [
-        (40, [hih, hih, hih, hih]),  # forward
-        (40, [mid, hih, mid, hih]),  # veer left
-        (40, [hih, hih, hih, hih]),  # forward
-        (40, [hih, mid, hih, mid]),  # veer right
-        (40, [off, off, off, off]),  # stop
-    ]
-    control_iter = iter(control_sequence)
-    current_control_steps, current_control = next(control_iter)
-    # tqdm.write(f"==>> Switching to next control: {current_control} for {current_control_steps} steps")
+    control_sequence = looping_control_sequence_generator(lo=0.0, mid=4.0, hi=12.0)
 
     observation, info = env.reset(seed=args.seed)
     # TODO: might still need to limit total number of steps (how is max_episode_steps handled in vectorised envs? or single environments)
     for step in trange(args.steps):
-        # action = env.action_space.sample()
-        action = current_control
+        action = next(control_sequence)
 
         observation, reward, terminated, truncated, info = env.step(action)
 
@@ -62,13 +79,6 @@ def main(args: Args) -> None:
         if terminated or truncated:
             observation, info = env.reset()
             break
-
-        current_control_steps -= 1
-        if current_control_steps <= 0:
-            try:
-                current_control_steps, current_control = next(control_iter)
-            except StopIteration:
-                current_control_steps = 20
 
     env.close()
 
