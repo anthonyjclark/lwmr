afec735ec3f6f829fa0a7ae5f0d735bb469fab6d
anthonyjclark <anthonyjclark@gmail.com>
2026-05-29 15:29:14 -0700

    Added loguru.

--- packages/lwmr/src/lwmr/__main__.py
+++ packages/lwmr/src/lwmr/__main__.py
@@ -2,11 +2,23 @@
 This file is mainly used to test the installation.
 """
 
+from dataclasses import dataclass
+
 import gymnasium as gym
+import tyro
 
 import lwmr  # noqa: F401 <-- register the environment
 
-env = gym.make("lwmr/Lwmr-v0", quiet=True)
+
+@dataclass
+class Args:
+    quiet: bool = False
+    device: str = "cpu"
+
+
+args = tyro.cli(Args)
+
+env = gym.make("lwmr/Lwmr-v0", quiet=args.quiet)
 env.reset()
 env.step(env.action_space.sample())
 env.close()
